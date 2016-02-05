ba052e7b9c208500c874bdbe5ccd829ccb47cc24
Robert Mann <robert_g_mann@omb.eop.gov>
2016-02-05 17:42:03 -0500

    firewall

---
+++ firewall.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# usage: sudo bash -x firewall.sh eth0
+
+iptables -F
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
+
+add_rules()
+{
+	# outgoing DNS
+	iptables -A OUTPUT -o $1 -p udp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	iptables -A INPUT -i $1 -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -o $1 -p tcp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	iptables -A INPUT -i $1 -p tcp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+	# outgoing ICMP
+	iptables -A OUTPUT -o $1 -p icmp -j ACCEPT
+	iptables -A INPUT -i $1 -p icmp -j ACCEPT
+
+	# outgoing HTTP
+	iptables -A OUTPUT -o $1 -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	iptables -A INPUT -i $1 -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	
+	# outgoing HTTPS
+	iptables -A OUTPUT -o $1 -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	iptables -A INPUT -i $1 -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+        # outgoing HTTPS
+        iptables -A OUTPUT -o $1 -p udp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+        iptables -A INPUT -i $1 -p udp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	
+	iptables -A OUTPUT -o $1 -p tcp --dport 9418 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+        iptables -A INPUT -i $1 -p tcp --sport 9418 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+        iptables -A OUTPUT -o $1 -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+        iptables -A INPUT -i $1 -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+}
+
+add_rules $1
+add_rules tun0
+
+iptables -A OUTPUT -o tun0 -p tcp --dport 8080 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i tun0 -p tcp --sport 8080 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+# log dropped connections
+iptables -A INPUT -j LOG  --log-prefix "IN-DROPPED "
+iptables -A OUTPUT -j LOG  --log-prefix "OUT-DROPPED "

63e10879758f0c7f2aa70d88f4032c1179e7490a
ramann <github@robeverybody.com>
2016-02-07 22:10:28 -0500

    pin file

--- connect.sh
+++ connect.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-(cat ~/.piv ; echo "AOMB") | sudo openconnect --base-mtu=$(ip link | grep ' UP ' | head -1 | awk '{print $5}') --cafile=/usr/local/etc/MAX_VPN_CA.pem -c 'pkcs11:id=%01' -s ~/devsetup/split.sh https://access3.max.gov/
+if [ ! -e ~/.pin ]
+then
+	touch ~/.pin
+	chmod 600 ~/.pin
+	echo -n "Enter smartcard PIN: "
+	read line
+	echo "$line" > ~/.pin
+fi
+
+(cat ~/.pin ; echo "AOMB") | sudo openconnect --base-mtu=$(ip link | grep ' UP ' | head -1 | awk '{print $5}') --cafile=/usr/local/etc/MAX_VPN_CA.pem -c 'pkcs11:id=%01' -s ~/devsetup/split.sh https://access3.max.gov/
